d33a2684e169daa6623e1c1792f6044d2919eb8e
BlueAlice56 <komotoayana56@gmail.com>
2023-11-02 20:05:06 +0900

    「UpdateServlet.javaを作成」C13

---
+++ src/controllers/UpdateServlet.java
@@ -0,0 +1,64 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task_dto;
+import utils.Task_dao;
+
+/**
+ * Servlet implementation class UpdateServlet
+ */
+@WebServlet("/update")
+public class UpdateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = Task_dao.createEntityManager();
+
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            //id は EditServlet でセッションスコープに保存した message_id のデータを使う。
+            //request.getSession().getAttribute() を使ってセッションスコープから取得したデータは、
+            //汎用的な Object 型になっているため、(Integer) へキャストする。
+            Task_dto m = em.find(Task_dto.class, (Integer)(request.getSession().getAttribute("message_id")));
+
+            // フォームの内容を各フィールドに上書き
+            String content = request.getParameter("content");
+            m.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            m.setUpdated_at(currentTime);       // 更新日時のみ上書き
+
+            // データベースを更新
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            em.close();
+
+            // 前のデータを削除
+            request.getSession().removeAttribute("message_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
+}
