b77b371d93afba7b46b19580aacee168628b907c
BlueAlice56 <komotoayana56@gmail.com>
2023-11-02 19:12:58 +0900

    「EditServlet.javaを作成」C12

---
+++ src/controllers/EditServlet.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task_dto;
+import utils.Task_dao;
+
+/**
+ * Servlet implementation class EditServlet
+ */
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = Task_dao.createEntityManager();
+
+     // 該当のIDのメッセージ1件のみをデータベースから取得
+        //request.getParameter("id")でURLに記載されたIDの価をString型で取得
+        //Integer.parseIntで、String型の価をint型に変える。
+        //整数の値でem.findメソッドで検索して、DTO変数に代入する。
+        Task_dto m = em.find(Task_dto.class, Integer.parseInt(request.getParameter("id")));
+
+        //接続を閉じる
+        em.close();
+
+        // メッセージ情報とセッションIDをリクエストスコープに登録
+        request.setAttribute("message", m);
+        request.setAttribute("_token", request.getSession().getId());
+
+        // メッセージIDをセッションスコープに登録
+        request.getSession().setAttribute("message_id", m.getId());
+
+        //遷移
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+        rd.forward(request, response);
+    }
+}
